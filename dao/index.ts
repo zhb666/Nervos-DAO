29a13e864d3c8839f5c8c0b3d465db0c4f780e21
zhb666 <462612421@qq.com>
2023-01-12 11:03:13 +0800

    feat: test

--- dao/index.ts
+++ dao/index.ts
@@ -8,9 +8,9 @@ async function capacity() {
 }
 
 async function deposit() {
-  const txhash = await daoDeposit(BigInt(186 * 10 ** 8), 1000);
+  const txhash = await daoDeposit(BigInt(188 * 10 ** 8), 1000);
   console.log(txhash);
 }
 
 capacity();
-deposit()
+deposit();
